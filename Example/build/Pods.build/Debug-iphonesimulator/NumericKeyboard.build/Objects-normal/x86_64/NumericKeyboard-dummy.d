443eb1264362a9c67d669fde9736a0dc8f3a454d
Liew Jun Tung <juntung@netvirta.com>
2018-11-27 17:26:35 +0800

    Try to add shared framework

---
+++ Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/Objects-normal/x86_64/NumericKeyboard-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/juntung/Dev/OpenSource/NumericKeyboard/Example/Pods/Target\ Support\ Files/NumericKeyboard/NumericKeyboard-dummy.m \
+  /Users/juntung/Dev/OpenSource/NumericKeyboard/Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/module.modulemap \
+  /Users/juntung/Dev/OpenSource/NumericKeyboard/Example/Pods/Target\ Support\ Files/NumericKeyboard/NumericKeyboard-prefix.pch
---
+++ Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/Objects-normal/x86_64/NumericKeyboard_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/juntung/Dev/OpenSource/NumericKeyboard/Example/build/Pods.build/Debug-iphonesimulator/NumericKeyboard.build/DerivedSources/NumericKeyboard_vers.c
